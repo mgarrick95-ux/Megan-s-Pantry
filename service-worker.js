2de678f1c181b6d8e82a54bed3756f304ad73da5
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-20 21:04:41 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = 'frostpantry-pages-v5'; // force refresh
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
